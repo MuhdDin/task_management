9b95fd68ecc6be0ff68d8da944f121f02601e001
MuhdDin <mdin2000@gmail.com>
2024-01-01 00:31:46 +0800

    initial commit

--- lib/features/auth/pages/login_page.dart
+++ lib/features/auth/pages/login_page.dart
@@ -1,6 +1,13 @@
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:task_manager/common/utils/constants.dart';
+import 'package:task_manager/common/widget/appstyle.dart';
+import 'package:task_manager/common/widget/custom_otn_btn.dart';
+import 'package:task_manager/common/widget/custom_text.dart';
+import 'package:task_manager/common/widget/heighspacer.dart';
+import 'package:task_manager/common/widget/reusable_text.dart';
 
 class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +17,19 @@ class LoginPage extends ConsumerStatefulWidget {
 }
 
 class _LoginPageState extends ConsumerState<LoginPage> {
+  final TextEditingController phone = TextEditingController();
+  Country country = Country(
+      phoneCode: "1",
+      countryCode: "US",
+      e164Sc: 0,
+      geographic: true,
+      level: 1,
+      name: "USA",
+      example: "USA",
+      displayName: "United States",
+      displayNameNoCountryCode: "US",
+      e164Key: "");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +44,56 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                 "assets/images/todo.png",
                 width: 300,
               ),
+            ),
+            const HeightSpacer(hieght: 20),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 16.w),
+              child: ReusableText(
+                text: "Please provide your Phone Number",
+                style: appstyle(17, AppConst.kLight, FontWeight.w500),
+              ),
+            ),
+            const HeightSpacer(hieght: 20),
+            Center(
+              child: CustomTextField(
+                controller: phone,
+                prefixIcon: Container(
+                  padding: const EdgeInsets.all(14),
+                  child: GestureDetector(
+                    onTap: () {
+                      showCountryPicker(
+                        context: context,
+                        countryListTheme: CountryListThemeData(
+                            backgroundColor: AppConst.kLight,
+                            bottomSheetHeight: AppConst.kHeight * 0.6,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(AppConst.kRadius),
+                                topRight: Radius.circular(AppConst.kRadius))),
+                        onSelect: (code) {
+                          setState(() {});
+                        },
+                      );
+                    },
+                    child: ReusableText(
+                        text: "${country.flagEmoji} + ${country.phoneCode}",
+                        style: appstyle(18, AppConst.kBkDark, FontWeight.bold)),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                hintText: "Enter phone number",
+                hintStyle: appstyle(16, AppConst.kBkDark, FontWeight.w600),
+              ),
+            ),
+            const HeightSpacer(hieght: 20),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: CustomOtlnBtn(
+                  width: AppConst.kWidth * 0.9,
+                  height: AppConst.kHeight * 0.075,
+                  color: AppConst.kBkDark,
+                  color2: AppConst.kLight,
+                  text: "Send Code"),
             )
           ],
         ),
